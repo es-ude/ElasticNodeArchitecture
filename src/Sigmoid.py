3ef5d706d32c4d6e90faa45140106381b104c7a5
Alwyn Burger <alwyn.burger@outlook.com>
2017-06-19 12:54:15 +0200

    finished fixing sigmoid shortening

--- src/Sigmoid.py
+++ src/Sigmoid.py
@@ -11,13 +11,13 @@ eps = 5.
 limit = 10.
 
 def float_sigmoid(x):
-	return 1. / (1. + np.exp(-x))
+	return eps + (factor - eps*2) * (1. / (1. + np.exp(-x)))
 
 def int_sigmoid(x):
         result = round((factor - 2*eps) * 1. / (1. + math.exp(-x)) + eps)
         # limit from edge
         result = max(eps, min(result, factor-eps))
-        print x, result
+        # print x, result
         return int(result)
 
 if __name__ == '__main__':
@@ -48,31 +48,43 @@ if __name__ == '__main__':
 
 
 	x = np.linspace(-limit, limit, 100)
-	y = float_sigmoid(x)* factor
+	y = float_sigmoid(x)
 	y2 = np.zeros_like(x)
 	r = np.zeros((2,))
 	y3 = list()
 	oldy = eps
-	for i in range(len(y2) - 1):
+	for i in range(len(y2)):
 		y2[i] = int_sigmoid(x[i])
 		if y2[i] != oldy:
 			oldy = y2[i]
 			r[1] = i
-			y3.append(np.array([r[0], r[1], oldy]))
-			r[0] = i+1
+			y3.append(np.array([r[0], r[1], oldy]).astype('int'))
+			r[0] = i
 		#output.append('		elsif arg >= to_signed(%d, fixed_point\'length) and arg < to_signed(%d, fixed_point\'length) then' % (int(factor * x[i]), int(factor * x[i+1])))
 		# output.append('			ret := to_signed(%d, fixed_point\'length);' % i)
 		#output.append('			ret := to_signed(%d, fixed_point\'length);' % y2[i])
+	# last entry:
+	y3.append(np.array([r[0], len(y2) - 1, y2[-1]]).astype('int'))
 
+	pp.figure()
+	pp.hold(True)
 
 	for i in range(len(y3)):
-		output.append('		elsif arg >= to_signed(%d, fixed_point\'length) and arg < to_signed(%d, fixed_point\'length) then' % (int(factor * x[y3[i][0]]), int(factor * y3[i][1])))
-		output.append('			ret := to_signed(%d, fixed_point\'length);' % y3[i][2])
+		current = y3[i]
+		print current,
+		current[0] = (int(factor * x[current[0]]))
+		current[1] = (int(factor * x[current[1]]))
+
+		print current
+		pp.plot([current[0], current[1]], [current[2], current[2]], 'b')
+		output.append('		elsif arg >= to_signed(%d, fixed_point\'length) and arg < to_signed(%d, fixed_point\'length) then' % (current[0], current[1]))
+		output.append('			ret := to_signed(%d, fixed_point\'length);' % current[2])
 	#print y3
 	
-	#pp.plot(x, np.array([y2,y]).T)
-	#pp.grid()
-	# pp.show()
+	pp.plot(factor * x, np.array([y2]).T, 'r')
+	pp.plot(factor * x, np.array([y]).T, 'g')
+	pp.grid()
+	pp.show()
 	
 	output.append('		else')
 	output.append('			return factor;')
