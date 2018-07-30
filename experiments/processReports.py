d8a2cc50f1757369df650f241205b869e491b7f4
Alwyn Burger <alwyn.burger@outlook.com>
2018-07-30 08:49:35 +0200

    monday morning

--- experiments/processReports.py
+++ experiments/processReports.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as pp
 import numpy as np 
 import os
 import xml.etree.ElementTree as ET
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter, FixedLocator
 
-def getvalues(dict, label):
+def getvalues(dict, table, label):
 	return values[table]['mw'][label][1], values[table]['ul'][label][0], values[table]['mm'][label][0]
+
 def processNode(node, dictionary):
 	if node.attrib['stringID'] == "MAP_MODULE":
 		key = node.attrib['value']
@@ -25,7 +27,7 @@ currentvalues = dict()
 key = None
 
 for i in range(1, 18):
-	# print 'processing', i
+	print 'processing', 'reports/{}.xrpt'.format(i)
 	tree = ET.parse('reports/{}.xrpt'.format(i))
 	root = tree.getroot()
 
@@ -52,35 +54,103 @@ dsp = np.zeros((len(values.keys()), 3))
 # 0 mw 1 skeleton 2 hwf
 # print values[1]
 for table in values:
-	luts[table-1, :] = getvalues(values, 'LUTs')
-	bram[table-1, :] = getvalues(values, 'BRAM/FIFO')
-	slices[table-1, :] = getvalues(values, 'Slices')
-	slicereg[table-1, :] = getvalues(values, 'Slice Reg')
-	dsp[table-1, :] = getvalues(values, 'DSP48A1')
+	luts[table-1, :] = getvalues(values, table, 'LUTs')
+	bram[table-1, :] = getvalues(values, table, 'BRAM/FIFO')
+	slices[table-1, :] = getvalues(values, table, 'Slices')
+	slicereg[table-1, :] = getvalues(values, table, 'Slice Reg')
+	dsp[table-1, :] = getvalues(values, table, 'DSP48A1')
 
-fig = pp.figure(figsize=(12,8))
-x = np.array(range(1,luts.shape[0]+1)) * 2 * 2 + 1 # 2x for 16bit 2x for both inputs + 1 control
-print x
-for i in range(3):
-	pp.subplot(1,3,i+1)
-	pp.plot(x, luts[:,i])
-	pp.plot(x, bram[:,i])
-	pp.plot(x, slices[:,i])
-	pp.plot(x, slicereg[:,i])
-	pp.plot(x, dsp[:,i])
+
+# print luts[14]
+# print slices[14]
+# print slicereg[14]
+# print dsp[14]
+
+# scale to relative consumption
+luts /= 5720. / 100.
+bram /= 64. / 100.
+slices /= 1430. / 100.
+slicereg /= 11440. / 100.
+dsp /= 16. / 100.
+
+print slicereg[-1, :]
+
+# fig = pp.figure(figsize=(8,3))
+# x = np.array(range(1,luts.shape[0]+1)) * 2 * 2 + 1 # 2x for 16bit 2x for both inputs + 1 control
+x = np.array(range(1, luts.shape[0] + 1))
+ # print x
+
+fsize = (6,1.8)
+majorLocator = FixedLocator(range(1, len(x), 5)) # MultipleLocator(5)
+majorFormatter = FormatStrFormatter('%d')
+yFormatter = FormatStrFormatter('%3d')
+minorLocator = MultipleLocator(1)
+
+fig, ax = pp.subplots(figsize=fsize) #3,1,1)
+pp.plot(x, luts, 'x-')
+pp.ylabel("Resource %");
+ax.xaxis.set_major_locator(majorLocator)
+ax.xaxis.set_major_formatter(majorFormatter)
+ax.xaxis.set_minor_locator(minorLocator)
+ax.yaxis.set_major_formatter(yFormatter)
+pp.ylim([0, 15])
+pp.title("LUTs")
+pp.legend(['Middleware', 'Skeleton', 'HWF'])
+pp.savefig('../../hpca2019/images/luts.pdf')
+
+# pp.subplot(312)
+fig, ax = pp.subplots(figsize=fsize) #3,1,1)
+pp.plot(x, slicereg, 'x-')
+pp.ylim([0, 6])
+pp.title("Slice registers")
+pp.ylabel("Resource %");
+ax.xaxis.set_major_locator(majorLocator)
+ax.xaxis.set_major_formatter(majorFormatter)
+ax.xaxis.set_minor_locator(minorLocator)
+ax.yaxis.set_major_formatter(yFormatter)
+pp.legend(['Middleware', 'Skeleton', 'HWF'])
+pp.savefig('../../hpca2019/images/slicereg.pdf')
+
+fig, ax = pp.subplots(figsize=fsize) #3,1,1)
+pp.plot(x, dsp, 'x-')
+pp.title("DSP")
+pp.ylim([0, 105])
+pp.ylabel("Resource %");
+ax.xaxis.set_major_locator(majorLocator)
+ax.xaxis.set_major_formatter(majorFormatter)
+ax.xaxis.set_minor_locator(minorLocator)
+ax.yaxis.set_major_formatter(yFormatter)
+pp.legend(['Middleware', 'Skeleton', 'HWF'])
+pp.savefig('../../hpca2019/images/dsp.pdf')
+
+
+# pp.legend(['Middleware', 'Skeleton', 'HWF'], bbox_to_anchor=(1,1), loc=2)
+
+pp.show()
+
+# mw, skel, hwf split
+# for i in range(3):
+# 	pp.subplot(1,3,i+1)
+# 	pp.plot(x, luts[:,i])
+# 	pp.plot(x, bram[:,i])
+# 	# pp.plot(x, slices[:,i])
+# 	pp.plot(x, slicereg[:,i])
+# 	pp.plot(x, dsp[:,i])
 	
-	pp.legend(['LUTs', 'BRAM', 'Slices', 'Slice Regs', 'DSP'])
-	pp.xlabel('Interface size (in bytes)')
-	pp.ylabel('Resource consumed')
-	pp.grid(axis='y')
-	pp.xticks(x[::2])
+# 	pp.legend(['LUTs', 'BRAM', 'Slice Regs', 'DSP']) # 'Slices', 
+# 	pp.xlabel('Interface size (in bytes)')
+# 	pp.ylabel('Resource consumed')
+# 	pp.ylim([0, 100])
+# 	pp.grid(axis='y')
+# 	pp.xticks(x[::2])
+# 	pp.yticks(range(0, 100, 10))
 
-pp.subplot(1,3,1)
-pp.title("Middleware")
-pp.subplot(1,3,2)
-pp.title("Skeleton")
-pp.subplot(1,3,3)
-pp.title("HWF")
+# pp.subplot(1,3,1)
+# pp.title("Middleware")
+# pp.subplot(1,3,2)
+# pp.title("Skeleton")
+# pp.subplot(1,3,3)
+# pp.title("HWF")
 # pp.show()
-pp.savefig('resources.pdf')
+
 
